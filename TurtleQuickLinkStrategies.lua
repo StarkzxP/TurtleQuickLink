7988b50c081a8f6b7cb827a8e2fcfff767b9dbe6
Starkz <starkzcc@gmail.com>
2025-06-03 02:56:05 -0400

    Update 1.1.0

--- TurtleQuickLinkStrategies.lua
+++ TurtleQuickLinkStrategies.lua
@@ -1,13 +1,16 @@
 TurtleQuickLink = TurtleQuickLink or {}
 
+local baseUrl = "%s?%s=%s"
+local turtleDBBaseUrl = "https://database.turtle-wow.org/"
+
 local strategies = {}
 local turtletooltipStates = {}
 
 function TurtleQuickLink:GetTurtleDBUrl(dataSources)
     for _, strategy in pairs(strategies) do
-        local id, type = strategy(dataSources)
-        if id and type then
-            return string.format("https://database.turtle-wow.org/?%s=%s", type, id)
+        local reference, type = strategy(dataSources)
+        if reference and type then
+            return string.format(baseUrl, turtleDBBaseUrl, type, reference)
         end
     end
 end
@@ -20,9 +23,62 @@ local function GetTypeAndIdFromLink(link)
     return id, type
 end
 
+local function GetQuestLogTitleIndex(str)
+    if type(str) ~= "string" then
+        return nil
+    end
+    local prefix = "QuestLogTitle"
+    local prefixLen = 13
+    local numStr = string.sub(str, prefixLen + 1)
+    if numStr and numStr ~= "" then
+        local number = tonumber(numStr)
+        return number
+    end
+    return nil
+end
+
+local function urlencode(url)
+    local char_to_hex = function(c)
+        return string.format("%%%02X", string.byte(c))
+    end
+    url = string.gsub(url, "\n", "\r\n")
+    url = string.gsub(url, "([^%w ])", char_to_hex)
+    url = string.gsub(url, " ", "+")
+    return url
+end
+
 function strategies.GetItemFromTooltip(data)
     if not data.tooltip then
-        return
+        return nil
     end
     return GetTypeAndIdFromLink(data.tooltip.itemLink)
+end
+
+function strategies.GetItemFromItemRef(data)
+    if data.focus and data.focus.itemID then
+        return data.focus.itemID, "item"
+    end
+    return nil
+end
+
+function strategies.GetQuestTitleFromQuestLog(data)
+    if not data.focus then
+        return nil
+    end
+    local questIndex = GetQuestLogTitleIndex(data.focus:GetName())
+    if not questIndex then
+        return nil
+    end
+    local questTitle, _, _, isHeader = GetQuestLogTitle(questIndex)
+    if not questTitle or isHeader then
+        return nil
+    end
+    return urlencode(questTitle), "search"
+end
+
+function strategies.GetQuestFromPfQuestTracker(data)
+    if data.focus and data.focus.questid then
+        return data.focus.questid, "quest"
+    end
+    return nil
 end
